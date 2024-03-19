bb677158356d687f95148d33a68aef6d60cbc86c
Gabriel Fernandes <113441631+BielFernandes@users.noreply.github.com>
2024-03-19 19:14:33 -0300

    fix to production environment (#3)

--- src/app/home/cadastro/obras/newpage/page.tsx
+++ src/app/home/cadastro/obras/newpage/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import CreateUnipDropdown from "@/components/constructionRegister/CreateUnityDropdown";
 import { useConstruction } from "@/contexts/ConstructionsContext";
 import { useMacro } from "@/contexts/MacroservicesContext";
@@ -16,7 +16,7 @@ interface Unit {
   quantidade: number;
   peso: number;
   isDisabled: boolean;
-  sub_id: any
+  sub_id: any;
 }
 
 export default function Newpage() {
@@ -30,19 +30,18 @@ export default function Newpage() {
   const mutation = useMutation({
     mutationFn: (formData: any) => postConstruction(formData),
     onSuccess: (data) => {
-      console.log(data)
+      console.log(data);
     },
     onError: (error) => {
-      console.error(error)
+      console.error(error);
     },
-
   });
 
   useEffect(() => {
-    if (constructionData.unidades_individual && subservice && data) {
+    if (constructionData && constructionData.unidades_individual && subservice && data) {
       const updatedUnidades = constructionData.unidades_individual.map((unit: any) => {
         const matchingSub = subservice.find((sub: any) => sub.id === unit.sub_id);
-        const matchingMacro = data.find((macro: any) => macro.id === matchingSub.macro_id);
+        const matchingMacro = data.find((macro: any) => macro.id === (matchingSub ? matchingSub.macro_id : null));
         if (matchingSub && matchingMacro) {
           return {
             ...unit,
@@ -50,7 +49,7 @@ export default function Newpage() {
             macro_name: matchingMacro.macroservice,
             quantidade: 0,
             peso: 0,
-            isDisabled: false
+            isDisabled: false,
           };
         } else {
           return unit;
@@ -68,13 +67,13 @@ export default function Newpage() {
 
       setGroupedDataIndividual(grouped);
     }
-  }, [constructionData.unidades_individual, subservice, data]);
+  }, [constructionData, subservice, data]);
 
   useEffect(() => {
-    if (constructionData.unidades_repeticao && subservice && data) {
+    if (constructionData && constructionData.unidades_repeticao && subservice && data) {
       const updatedUnidades = constructionData.unidades_repeticao.map((unit: any) => {
         const matchingSub = subservice.find((sub: any) => sub.id === unit.sub_id);
-        const matchingMacro = data.find((macro: any) => macro.id === matchingSub.macro_id);
+        const matchingMacro = data.find((macro: any) => macro.id === (matchingSub ? matchingSub.macro_id : null));
         if (matchingSub && matchingMacro) {
           return {
             ...unit,
@@ -82,7 +81,7 @@ export default function Newpage() {
             macro_name: matchingMacro.macroservice,
             quantidade: 0,
             peso: 0,
-            isDisabled: false
+            isDisabled: false,
           };
         } else {
           return unit;
@@ -100,14 +99,14 @@ export default function Newpage() {
 
       setGroupedDataRepeticao(grouped);
     }
-  }, [constructionData.unidades_repeticao, subservice, data]);
+  }, [constructionData, subservice, data]);
 
   const handleOKClickIndividual = (unit: Unit, place: string, quantidade: number, peso: number) => {
     const updatedUnit: Unit = {
       ...unit,
       quantidade: quantidade,
       peso: peso,
-      isDisabled: true
+      isDisabled: true,
     };
 
     const updatedGroupedData: { [key: string]: Unit[] } = { ...groupedDataIndividual };
@@ -124,7 +123,7 @@ export default function Newpage() {
       ...unit,
       quantidade: quantidade,
       peso: peso,
-      isDisabled: true // Definindo isDisabled como true
+      isDisabled: true, // Definindo isDisabled como true
     };
 
     const updatedGroupedData: { [key: string]: Unit[] } = { ...groupedDataRepeticao };
@@ -132,180 +131,160 @@ export default function Newpage() {
       item === unit ? updatedUnit : item
     );
 
-    constructionData.units.forEach((unitie: string) => {
-      if (unit.place == unitie) {
-        return
-      }
-      unit.place = unitie
-      const updatedUnit: Unit = {
-        ...unit,
-        quantidade: quantidade,
-        peso: peso,
-        isDisabled: true
-      };
-
-      const updatedGroupedData: { [key: string]: Unit[] } = { ...groupedDataRepeticao };
-      updatedGroupedData[ambient] = updatedGroupedData[ambient].filter((item: Unit) => item !== unit);
-      setGroupedDataRepeticao(updatedGroupedData);
-
-      setStoredData((prevStoredData: Unit[]) => [...prevStoredData, updatedUnit]);
-    })
+    // Remove o item de um lugar e adiciona em outro
+    const newStoredData = storedData.filter((storedUnit) => storedUnit !== unit);
+    setStoredData((prevStoredData: Unit[]) => [...newStoredData, updatedUnit]);
 
     setGroupedDataRepeticao(updatedGroupedData);
-
-    setStoredData((prevStoredData: Unit[]) => [...prevStoredData, updatedUnit]);
   };
 
   useEffect(() => {
-    console.log(storedData)
+    console.log(storedData);
   }, [storedData]);
 
   const teste = () => {
-    const newData = storedData.map(({ place, ambient, sub_id }) => ({ place, ambient, sub_id }))
-    console.log('newData', newData)
-    const newConstructionData = constructionData
-    newConstructionData.unidades_repeticao = storedData
-    mountBody(newConstructionData)
-    console.log()
-    mutation.mutate(newConstructionData)
-  }
+    const newConstructionData = {
+      ...constructionData,
+      unidades_repeticao: storedData,
+    };
+    mountBody(newConstructionData);
+    mutation.mutate(newConstructionData);
+  };
 
   return (
     <div className="w-screen h-auto flex justify-center items-center p-20">
       <div className="w-full max-w-screen-lg">
-        <CreateUnipDropdown
-          children={
-            <div >
-              <p className="text-center mb-4">Unidades individuais agrupadas por local</p>
-              {Object.entries(groupedDataIndividual).map(([place, units], index) => (
-                <div key={index}>
-                  <h2 className="text-center mb-2">{`Local: ${place}`}</h2>
-                  <table className="w-full mb-4 border">
-                    <thead>
-                      <tr>
-                        <th>Ambiente</th>
-                        <th>Macro Service</th>
-                        <th>Subservice</th>
-                        <th>Quantidade</th>
-                        <th>Unidade</th>
-                        <th>Ação</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {units &&
-                        units.map((item: Unit, idx: number) => (
-                          <tr key={idx} className={item.isDisabled ? 'opacity-50 pointer-events-none' : 'opacity-100'}>
-                            <td>{item.ambient}</td>
-                            <td>{item.macro_name}</td>
-                            <td>{item.name}</td>
-                            <td>
-                              <input
-                                type="number"
-                                defaultValue={item.quantidade}
-                                onChange={(e) => {
-                                  const value = parseInt(e.target.value);
-                                  setGroupedDataIndividual(prevGroupedData => {
-                                    const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
-                                    updatedGroupedData[place][idx].quantidade = value;
-                                    return updatedGroupedData;
-                                  });
-                                }}
-                              />
-                            </td>
-                            <td>
-                              <input
-                                type="number"
-                                defaultValue={item.peso}
-                                onChange={(e) => {
-                                  const value = parseInt(e.target.value);
-                                  setGroupedDataIndividual(prevGroupedData => {
-                                    const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
-                                    updatedGroupedData[place][idx].peso = value;
-                                    return updatedGroupedData;
-                                  });
-                                }}
-                              />
-                            </td>
-                            <td>
-                              <button onClick={() => handleOKClickIndividual(item, place, item.quantidade, item.peso)}>OK</button>
-                            </td>
-                          </tr>
-                        ))}
-                    </tbody>
-                  </table>
-                </div>
-              ))}
-            </div>
-          }
+        <CreateUnipDropdown>
+          <div>
+            <p className="text-center mb-4">Unidades individuais agrupadas por local</p>
+            {Object.entries(groupedDataIndividual).map(([place, units], index) => (
+              <div key={index}>
+                <h2 className="text-center mb-2">{`Local: ${place}`}</h2>
+                <table className="w-full mb-4 border">
+                  <thead>
+                    <tr>
+                      <th>Ambiente</th>
+                      <th>Macro Service</th>
+                      <th>Subservice</th>
+                      <th>Quantidade</th>
+                      <th>Unidade</th>
+                      <th>Ação</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {units &&
+  units.map((item: Unit, idx: number) => (
+    <tr key={idx} className={item.isDisabled ? 'opacity-50 pointer-events-none' : 'opacity-100'}>
+      <td className="border">{item.ambient}</td>
+      <td className="border">{item.macro_name}</td>
+      <td className="border">{item.name}</td>
+      <td className="border">
+        <input
+          type="number"
+          defaultValue={item.quantidade}
+          onChange={(e) => {
+            const value = parseInt(e.target.value);
+            setGroupedDataIndividual((prevGroupedData) => {
+              const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
+              updatedGroupedData[place][idx].quantidade = value;
+              return updatedGroupedData;
+            });
+          }}
         />
-        <CreateUnipDropdown
-          children={
-            <>
-              <p className="text-center mb-4">Unidades de repetição agrupadas por ambiente</p>
-              {Object.entries(groupedDataRepeticao).map(([ambient, units], index) => (
-                <div key={index}>
-                  <h2 className="text-center mb-2">{`Ambiente: ${ambient}`}</h2>
-                  <table className="w-full mb-4 border">
-                    <thead>
-                      <tr>
-                        <th>Macro Service</th>
-                        <th>Subservice</th>
-                        <th>Quantidade</th>
-                        <th>Unidade</th>
-                        <th>Ação</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {units &&
-                        units.map((item: Unit, idx: number) => {
-                          if (idx > 0 && units[idx - 1].name === item.name) {
-                            return null;
-                          }
-                          return (
-                            <tr key={idx} className={item.isDisabled ? 'opacity-50 pointer-events-none' : 'opacity-100'}>
-                              <td>{item.macro_name}</td>
-                              <td>{item.name}</td>
-                              <td>
-                                <input
-                                  type="number"
-                                  defaultValue={item.quantidade}
-                                  onChange={(e) => {
-                                    const value = parseInt(e.target.value);
-                                    setGroupedDataRepeticao(prevGroupedData => {
-                                      const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
-                                      updatedGroupedData[ambient][idx].quantidade = value;
-                                      return updatedGroupedData;
-                                    });
-                                  }}
-                                />
-                              </td>
-                              <td>
-                                <input
-                                  type="number"
-                                  defaultValue={item.peso}
-                                  onChange={(e) => {
-                                    const value = parseInt(e.target.value);
-                                    setGroupedDataRepeticao(prevGroupedData => {
-                                      const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
-                                      updatedGroupedData[ambient][idx].peso = value;
-                                      return updatedGroupedData;
-                                    });
-                                  }}
-                                />
-                              </td>
-                              <td>
-                                <button onClick={() => handleOKClickRepeticao(item, ambient, item.quantidade, item.peso)}>OK</button>
-                              </td>
-                            </tr>
-                          );
-                        })}
-                    </tbody>
-                  </table>
-                </div>
-              ))}
-            </>
-          }
+      </td>
+      <td className="border">
+        <input
+          type="number"
+          defaultValue={item.peso}
+          onChange={(e) => {
+            const value = parseInt(e.target.value);
+            setGroupedDataIndividual((prevGroupedData) => {
+              const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
+              updatedGroupedData[place][idx].peso = value;
+              return updatedGroupedData;
+            });
+          }}
         />
+      </td>
+      <td className="border">
+        <button onClick={() => handleOKClickIndividual(item, place, item.quantidade, item.peso)}>OK</button>
+      </td>
+    </tr>
+  ))}
+
+                  </tbody>
+                </table>
+              </div>
+            ))}
+          </div>
+        </CreateUnipDropdown>
+        <CreateUnipDropdown>
+          <div>
+            <p className="text-center mb-4">Unidades de repetição agrupadas por ambiente</p>
+            {Object.entries(groupedDataRepeticao).map(([ambient, units], index) => (
+              <div key={index}>
+                <h2 className="text-center mb-2">{`Ambiente: ${ambient}`}</h2>
+                <table className="w-full mb-4 border">
+                  <thead>
+                    <tr>
+                      <th>Macro Service</th>
+                      <th>Subservice</th>
+                      <th>Quantidade</th>
+                      <th>Unidade</th>
+                      <th>Ação</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {units &&
+  units.map((item: Unit, idx: number) => {
+    if (idx > 0 && units[idx - 1].name === item.name) {
+      return null;
+    }
+    return (
+      <tr key={idx} className={item.isDisabled ? 'opacity-50 pointer-events-none' : 'opacity-100'}>
+        <td className="border">{item.macro_name}</td>
+        <td className="border">{item.name}</td>
+        <td className="border">
+          <input
+            type="number"
+            defaultValue={item.quantidade}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+              setGroupedDataRepeticao((prevGroupedData) => {
+                const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
+                updatedGroupedData[ambient][idx].quantidade = value;
+                return updatedGroupedData;
+              });
+            }}
+          />
+        </td>
+        <td className="border">
+          <input
+            type="number"
+            defaultValue={item.peso}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+              setGroupedDataRepeticao((prevGroupedData) => {
+                const updatedGroupedData: { [key: string]: Unit[] } = { ...prevGroupedData };
+                updatedGroupedData[ambient][idx].peso = value;
+                return updatedGroupedData;
+              });
+            }}
+          />
+        </td>
+        <td className="border">
+          <button onClick={() => handleOKClickRepeticao(item, ambient, item.quantidade, item.peso)}>OK</button>
+        </td>
+      </tr>
+    );
+  })}
+
+                  </tbody>
+                </table>
+              </div>
+            ))}
+          </div>
+        </CreateUnipDropdown>
         <button type="button" onClick={teste}><p>Salvar sub-services</p></button>
       </div>
     </div>
